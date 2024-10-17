6480b198ddb3bb5f228c9856149176531d2f2126
git-seungwon <tmddnjs3369@naver.com>
2024-10-17 09:35:39 +0900

    setting 테이블 오류 수정 #1

---
+++ api_testing/tasks.py
@@ -0,0 +1,27 @@
+import requests
+
+def signup():
+    data = {
+    "username": "정승원테스트",
+    "password1": "qwer1234",
+    "password2": "qwer1234",
+    "email": "test_tmddnjs3369@naver.com",
+    "gender": True
+    }
+
+    req = requests.post("http://j3pbl.kro.kr/stack/api/v1/signup", json=data)
+    print(req.status_code)
+    print(req.json())
+
+def login():
+    data = {
+    "email": "test_tmddnjs3369@naver.com",
+    "password": "qwer1234"
+    }
+
+    req = requests.post("http://j3pbl.kro.kr/stack/api/v1/login", json=data)
+    print(req.status_code)
+    print(req.json())
+
+login()
+
