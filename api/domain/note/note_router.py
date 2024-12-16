c3ac9449b54a4c897ca5ee04f88e9de05d3d7d48
git-seungwon <tmddnjs3369@naver.com>
2024-12-16 11:25:16 +0900

    1

--- api/domain/note/note_router.py
+++ api/domain/note/note_router.py
@@ -73,8 +73,6 @@ async def get_note(note_id: int, db: AsyncSession = Depends(get_db),
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail="권한이 없습니다.")
     
-    
-
     return note_schema.NoteResponse(
         note_id=note.note_id,
         user_id=note.user_id,
